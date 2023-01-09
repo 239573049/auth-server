044dc72b8bf15934c750bf5edeb51a5904f57961
token <239573049@qq.com>
2023-01-10 01:09:53 +0800

    增加入场动画

--- web/src/pages/admin/home/index.tsx
+++ web/src/pages/admin/home/index.tsx
@@ -2,6 +2,10 @@ import { Component, ReactNode } from "react";
 
 export default class Home extends Component {
     render(): ReactNode {
-        return (<div>home</div>)
+        return (<div key="userlist">
+                <div>
+                    测试
+                </div>
+             </div>)
     }
 }
