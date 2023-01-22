d2efb4a3271d894b0e35da546d5443fbf42bfc98
token <239573049@qq.com>
2023-01-22 15:30:06 +0800

    修改

--- web/src/apis/authserverApi.ts
+++ web/src/apis/authserverApi.ts
@@ -1,19 +1,26 @@
-import request from "@/utils/request";
+import request from '@/utils/request';
 
 class AuthServerApi {
-    token(username: string, password: string) {
-        return request.post('/connect/token', {
-            requestType: 'form',
-            data: {
-                grant_type: "password",
-                client_id: "Simple_App",
-                client_secret: null,
-                username: username,
-                scope: 'Simple',
-                password: password
-            }
-        })
-    }
+  token(
+    username: string,
+    password: string,
+    client_secret: string | null = null,
+    scope: string | null = 'Simple',
+    client_id: string | null = 'Simple_App',
+    grant_type: string | null = 'password',
+  ) {
+    return request.post('/connect/token', {
+      requestType: 'form',
+      data: {
+        grant_type: grant_type,
+        client_id: client_id,
+        client_secret: client_secret,
+        username: username,
+        scope: scope,
+        password: password,
+      },
+    });
+  }
 }
 
-export default new AuthServerApi;
+export default new AuthServerApi();
--- web/src/pages/user/login.tsx
+++ web/src/pages/user/login.tsx
@@ -1,112 +1,124 @@
 import authserverApi from '@/apis/authserverApi';
 import { GetQueryValue } from '@/utils/locationhelper';
 import {
-    GithubOutlined,
-    LockOutlined,
-    TaobaoCircleOutlined,
-    UserOutlined,
-    WeiboCircleOutlined,
+  GithubOutlined,
+  LockOutlined,
+  TaobaoCircleOutlined,
+  UserOutlined,
+  WeiboCircleOutlined,
 } from '@ant-design/icons';
 import {
-    LoginForm,
-    ProFormCheckbox,
-    ProFormText,
+  LoginForm,
+  ProFormCheckbox,
+  ProFormText,
 } from '@ant-design/pro-components';
 import { Space, message } from 'antd';
 import { CSSProperties, useState } from 'react';
 import { history } from 'umi';
-import './login.less'
+import './login.less';
 
 const iconStyles: CSSProperties = {
-    marginLeft: '16px',
-    color: 'rgba(0, 0, 0, 0.2)',
-    fontSize: '24px',
-    verticalAlign: 'middle',
-    cursor: 'pointer',
+  marginLeft: '16px',
+  color: 'rgba(0, 0, 0, 0.2)',
+  fontSize: '24px',
+  verticalAlign: 'middle',
+  cursor: 'pointer',
 };
 
-
 export default () => {
-    function onSubmit(value: any) {
-        authserverApi.token(value.accountNumber, value.password)
-            .then(res => {
-                var returnUrl = GetQueryValue("ReturnUrl")
-                if(returnUrl){
-                    window.location.href = returnUrl+"?token="+res.access_token;
-                }
-                setToken(res.access_token)
-                message.success("登录成功！")
-                history.push("/")
-            })
-    }
+  function onSubmit(value: any) {
+    var client_id = GetQueryValue('client_id');
+    var scope = GetQueryValue('scope');
+    var returnUrl = GetQueryValue('redirect_uri');
+    var grant_type = GetQueryValue('grant_type');
+    var client_secret = GetQueryValue('grant_type');
 
-    function setToken(value: string) {
-        window.localStorage.setItem('token', value)
-    }
+    authserverApi
+      .token(
+        value.accountNumber,
+        value.password,
+        client_secret,
+        scope,
+        client_id,
+        grant_type,
+      )
+      .then((res) => {
+        if (returnUrl) {
+          window.location.href = returnUrl + '?token=' + res.access_token;
+        }
+        setToken(res.access_token);
+        message.success('登录成功！');
+        history.push('/');
+      });
+  }
 
-    return (
-        <div style={{ backgroundColor: 'white', }} className="plan" key="login">
-            <LoginForm
-                logo="https://blog-simple.oss-cn-shenzhen.aliyuncs.com/logo.png"
-                title="授权中心管理平台"
-                subTitle="简单好用的授权中心管理平台"
-                onFinish={async (value: any) => {
-                    onSubmit(value);
-                }}
-                actions={
-                    <Space>
-                        其他登录方式
-                        <GithubOutlined style={iconStyles} />
-                        <TaobaoCircleOutlined style={iconStyles} />
-                        <WeiboCircleOutlined style={iconStyles} />
-                    </Space>
-                }
-            >
-                <ProFormText
-                    name="accountNumber"
-                    fieldProps={{
-                        size: 'large',
-                        prefix: <UserOutlined className={'prefixIcon'} />,
-                    }}
-                    placeholder={'用户名: admin'}
-                    rules={[
-                        {
-                            required: true,
-                            message: '请输入用户名!',
-                        },
-                    ]}
-                />
-                <ProFormText.Password
-                    name="password"
-                    fieldProps={{
-                        size: 'large',
-                        prefix: <LockOutlined className={'prefixIcon'} />,
-                    }}
-                    placeholder={'密码: dd666666'}
-                    rules={[
-                        {
-                            required: true,
-                            message: '请输入密码！',
-                        },
-                    ]}
-                />
-                <div
-                    style={{
-                        marginBottom: 24,
-                    }}
-                >
-                    <ProFormCheckbox noStyle name="autoLogin">
-                        自动登录
-                    </ProFormCheckbox>
-                    <a
-                        style={{
-                            float: 'right',
-                        }}
-                    >
-                        忘记密码
-                    </a>
-                </div>
-            </LoginForm>
+  function setToken(value: string) {
+    window.localStorage.setItem('token', value);
+  }
+
+  return (
+    <div style={{ backgroundColor: 'white' }} className="plan" key="login">
+      <LoginForm
+        logo="https://blog-simple.oss-cn-shenzhen.aliyuncs.com/logo.png"
+        title="授权中心管理平台"
+        subTitle="简单好用的授权中心管理平台"
+        onFinish={async (value: any) => {
+          onSubmit(value);
+        }}
+        actions={
+          <Space>
+            其他登录方式
+            <GithubOutlined style={iconStyles} />
+            <TaobaoCircleOutlined style={iconStyles} />
+            <WeiboCircleOutlined style={iconStyles} />
+          </Space>
+        }
+      >
+        <ProFormText
+          name="accountNumber"
+          fieldProps={{
+            size: 'large',
+            prefix: <UserOutlined className={'prefixIcon'} />,
+          }}
+          placeholder={'用户名: admin'}
+          rules={[
+            {
+              required: true,
+              message: '请输入用户名!',
+            },
+          ]}
+        />
+        <ProFormText.Password
+          name="password"
+          fieldProps={{
+            size: 'large',
+            prefix: <LockOutlined className={'prefixIcon'} />,
+          }}
+          placeholder={'密码: dd666666'}
+          rules={[
+            {
+              required: true,
+              message: '请输入密码！',
+            },
+          ]}
+        />
+        <div
+          style={{
+            marginBottom: 24,
+          }}
+        >
+          <ProFormCheckbox noStyle name="autoLogin">
+            自动登录
+          </ProFormCheckbox>
+          <a
+            style={{
+              float: 'right',
+            }}
+          >
+            忘记密码
+          </a>
         </div>
-    );
+      </LoginForm>
+    </div>
+  );
 };
